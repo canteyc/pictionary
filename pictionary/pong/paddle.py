319dfa969acee18c994ce4d694647495c9298663
Cory <corycantey@gmail.com>
2023-05-16 11:01:26 -0400

    Create pong game from kivy tutorial

---
+++ pictionary/pong/paddle.py
@@ -0,0 +1,17 @@
+from kivy.properties import NumericProperty
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
+
+from pictionary.pong import PongBall
+
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball: PongBall):
+        if self.collide_widget(ball):
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced: Vector = Vector(-ball.velocity_x, ball.velocity_y) * 1.25
+            if abs(bounced.x) > ball.max_speed:
+                bounced *= ball.max_speed / abs(bounced.x)
+            ball.velocity = bounced.x, bounced.y + offset
