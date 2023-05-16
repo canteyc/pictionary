13af7b8aaf66d87c3a84250ee0d4f9fbad032faa
Cory <corycantey@gmail.com>
2023-05-16 11:23:09 -0400

    Add simple paint app from kivy tutorial

---
+++ pictionary/paint/__init__.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+from pictionary.paint.paint import PaintWidget
+
+
+class PaintApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.painter = None
+
+    def build(self):
+        parent = Widget()
+        self.painter = PaintWidget()
+        clear_button = Button(text='Clear')
+        clear_button.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clear_button)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
+
+
+if __name__ == '__main__':
+    PaintApp().run()
